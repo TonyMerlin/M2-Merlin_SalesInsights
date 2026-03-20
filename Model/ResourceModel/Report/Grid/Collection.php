c63236f6f11d4b77f69cd40a082510f5244b052d
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-20 16:27:18 +0000

    Create Collection.php

---
+++ Model/ResourceModel/Report/Grid/Collection.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Merlin\SalesInsights\Model\ResourceModel\Report\Grid;
+
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+
+class Collection extends SearchResult
+{
+}
