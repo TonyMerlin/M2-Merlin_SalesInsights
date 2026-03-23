92546fc204b03bad7c1be3cdcd3e01b166cce4a7
Tony Merlin <60714862+TonyMerlin@users.noreply.github.com>
2026-03-23 13:52:07 +0000

    Update Collection.php

--- Model/ResourceModel/Report/Grid/Collection.php
+++ Model/ResourceModel/Report/Grid/Collection.php
@@ -3,8 +3,40 @@ declare(strict_types=1);
 
 namespace Merlin\SalesInsights\Model\ResourceModel\Report\Grid;
 
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
+use Magento\Framework\Data\Collection\EntityFactoryInterface;
+use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+use Psr\Log\LoggerInterface;
 
 class Collection extends SearchResult
 {
+    public function __construct(
+        EntityFactoryInterface $entityFactory,
+        LoggerInterface $logger,
+        FetchStrategyInterface $fetchStrategy,
+        ManagerInterface $eventManager,
+        $mainTable = 'merlin_salesinsights_aggregate',
+        $resourceModel = \Merlin\SalesInsights\Model\ResourceModel\Report::class,
+        $identifierName = 'entity_id',
+        $connectionName = null
+    ) {
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $mainTable,
+            $resourceModel,
+            $identifierName,
+            $connectionName
+        );
+    }
+
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+        $this->addFieldToFilter('period_type', 'daily');
+        return $this;
+    }
 }
